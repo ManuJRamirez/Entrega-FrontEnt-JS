68857a45706bcdee981967a3d39ceb118ea124ff
Manuel Ramirez <manramloz@gmail.com>
2023-10-23 20:27:50 +0200

    añado tools con sparrestApi y metodos get,remove,createAcc

---
+++ webModules/signup/signupModel.js
@@ -0,0 +1,9 @@
+export const createrAccount = async (email, password) => {
+    const url = "auth/register";
+
+    const data = {
+        usermail : email,
+        password : password,
+    }
+    
+}
---
+++ webModules/tools/sparrestApi.js
@@ -0,0 +1,79 @@
+export const sparrestApi = () => {
+  const baseUrl = "http://localhost:8000/";
+
+  const get = async (endpoint) => {
+    const url = baseUrl + endpoint;
+    let response;
+    try {
+      response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        const message = data.message || 'Ha ocurrido un error';
+        throw new Error(message);
+      }
+    } catch (error) {
+      throw error.message;
+    }
+  };
+
+  const remove = async (endpoint) => {
+    const url = baseUrl + endpoint;
+    const token = localStorage.getItem('token');
+    let response;
+    try {
+      response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      
+      if (!response.ok) {
+        const data = await response.json()
+        const message = data.message || 'No ha sido posible borrar el elemento';
+        throw new Error(message);
+      }
+    } catch (error) {
+      if (error.message) {
+        throw error.message;
+      } else {
+        throw error;
+      }
+    }
+  };
+  const createAcc = async (endpoint) => {
+    const url = baseUrl + endpoint;
+
+    let response;
+    try {
+      response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          'Content-type': 'application/json',
+        }
+      });
+      
+      if (!response.ok) {
+        const data = await response.json()
+        const message = data.message || 'No ha sido posible registar la cuenta';
+        throw new Error(message);
+      }
+    } catch (error) {
+      if (error.message) {
+        throw error.message;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  return {
+    get: get,
+    delete: remove,
+    createAcc: createAcc, 
+  }
+}
